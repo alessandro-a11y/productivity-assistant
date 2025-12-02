e15d7e2d2f0b3644d053e5d0c4e38c2dbb626696
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2025-12-01 22:23:38 -0300

    ´

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://productivity-assistant-backend.onrender.com/"; 
+const API_BASE_URL = "https://productivity-assistant-backend.onrender.com"; 
 let tarefas = [];
 
 document.addEventListener('DOMContentLoaded', () => {
